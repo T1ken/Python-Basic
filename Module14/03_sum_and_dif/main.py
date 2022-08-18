5c62d3ce36ab1e1c925e6b765fdbe1344fe5eff5
DaimXlink <100219344+DaimXlink@users.noreply.github.com>
2022-08-18 13:35:44 +0300

    Начальника, принимай работа пожалуйста

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -2,18 +2,19 @@ def search_summ(num):
     summ_num = 0
     for i in str(num):
         summ_num += int(i)
-    print(summ_num)
+    return summ_num
 
 
 def search_num(num):
     summ_digit = 0
     for i in str(num):
         summ_digit += 1
-    print(summ_digit)
+    return summ_digit
 
 
 ask = int(input())
-search_summ(ask)
-search_num(ask)
+print(search_summ(ask))
+print(search_num(ask))
+print(search_summ(ask) - search_num(ask))
 
 # Во-первых, опять принты и инпуты без информации. Во вторых, разность суммы и количества цифр не выводится.
--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,6 +1,11 @@
 num_1 = input('Введите первое число: ')
 asd = list(reversed(num_1))
 qwe = ''.join(asd)
+num_2 = input('Введите первое число: ')
+ase = list(reversed(num_2))
+zxc = ''.join(ase)
 print(qwe)
+print(zxc)
+print(float(qwe) + float(zxc))
 
 # Найс, запрашивается всего 1 число. Чел харош.
