c157650418d71248de530ec1ae6fb1c49bd3884a
DaimXlink <100219344+DaimXlink@users.noreply.github.com>
2022-09-06 19:39:46 +0300

    я знаю как делать 5 таск

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,15 +1,13 @@
 origin_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 dict_pre = {}
-new_list = []
 for i in origin_list[0::2]:
     if len(dict_pre) == 0:
         dict_pre = {i: i + 1}
     else:
-        dict_pre.update({i: i+1})
-for k, v in dict_pre.items():
-    new_list.append(k)
-    new_list.append(v)
-print(dict_pre)
+        dict_pre.update({i: i + 1})
+tuple_list = [(key, value) for key, value in dict_pre.items()]
+print(origin_list)
+print(tuple_list)
 
 # В консоли выводится словарь, а должен быть список с кортежами. Я попробовал поставить в принт переменную new_list, но
 # тогда выводится копия оригинального списка. Исправляй
