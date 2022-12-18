4053cd480e818257c27fb27a13bec632c3685851
DaimXlink <100219344+DaimXlink@users.noreply.github.com>
2022-12-18 22:42:20 +0300

    Tempora mutantur etnos,mutamur in ilis

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,17 +1,21 @@
 import zipfile
 num = 0
+ascii_en = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+ascii_ru = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+ascii_ = ascii_ru + ascii_en
 dictionary = {}
 with zipfile.ZipFile("voyna-i-mir.zip") as archive:
     for filename in archive.filelist:
         with archive.open(filename) as file:
             for letter in file.read().decode():
                 if letter not in dictionary:
+                    letter = letter.lower()
                     dictionary[letter] = 0
                 dictionary[letter] += 1
                 num += 1
-
-print(num)
 for letter, amount in dictionary.items():
-    print(letter, amount)
+    letter.lower()
+    if letter in ascii_:
+        print(letter, amount)
 # толстой, ты ебанутый
 # я кст тоже, у меня в книге больше 100%
