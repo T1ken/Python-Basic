55a50de50d3f8f24d753dc517b98be4df60bd31b
DaimXlink <100219344+DaimXlink@users.noreply.github.com>
2022-12-19 22:47:37 +0300

    Tempora mutantur etnos,mutamur in ilis

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -2,20 +2,23 @@ import zipfile
 num = 0
 ascii_en = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 ascii_ru = [chr(i) for i in range(ord('а'), ord('я') + 1)]
-ascii_ = ascii_ru + ascii_en
+AScil_en = [chr(i) for i in range(ord('А'), ord('Я') + 1)]
+AScil_ru = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+ascii_ = ascii_ru + ascii_en + AScil_ru + AScil_en
 dictionary = {}
 with zipfile.ZipFile("voyna-i-mir.zip") as archive:
     for filename in archive.filelist:
         with archive.open(filename) as file:
             for letter in file.read().decode():
                 if letter not in dictionary:
-                    letter = letter.lower()
+                    letter = letter
                     dictionary[letter] = 0
                 dictionary[letter] += 1
                 num += 1
+list1 = dict()
 for letter, amount in dictionary.items():
-    letter.lower()
     if letter in ascii_:
-        print(letter, amount)
-# толстой, ты ебанутый
-# я кст тоже, у меня в книге больше 100%
+        list1[letter] = amount
+q = {k: v for k, v in sorted(list1.items(), key=lambda item: item[1])}
+for k, l in q.items():
+    print(k, l)
